515aa2b77886ed0cd0bcec290c3ee9947357924d
sadia sehil <sadiasehil89@gmail.com>
2018-12-24 17:54:25 -0500

    simple modification

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -2,16 +2,29 @@ package math.problems;
 
 import org.testng.Assert;
 
+import java.util.Scanner;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
         //testing Fcatorial
        int fact =  Factorial.main(5);
-        Assert.assertEquals(fact, 120 );
+       int number =120;
+
+        try {
+            Assert.assertEquals(fact, number, "factorial of 5 is not equals to "+number);
+        }catch(Exception ex){
+            ex.getMessage();
+        }
        //testing findLowestDifference
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
         int lowest =FindLowestDifference.main( array1,array2);
-        Assert.assertEquals(lowest,1);
+        int low = 5;
+        try {
+            Assert.assertEquals(lowest, low, "the Lowest Dufference is not  "+low);
+        }catch(Exception ex){
+            ex.getMessage();
+        }
     }
 }
